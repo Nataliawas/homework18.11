903fd0508168064277c0fa4e230c5e3925f36610
nataliawas <nataliawas@wp.pl>
2018-11-27 19:23:01 +0100

    files added

---
+++ task1/script.js
@@ -0,0 +1,3 @@
+var date=new Date();
+helloString="Hello! today is "+ date.getUTCDate() +"." + (date.getUTCMonth()+1) + "."+date.getUTCFullYear() +" "+ (date.getUTCHours()+1) +":"+ date.getUTCMinutes()+".";
+helloString;
